c82cb0d69aaf32833fdf636d84056e75bc8a4b10
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-06-06 22:01:47 +0300

    show article by id

--- js/article.js
+++ js/article.js
@@ -1,9 +1,22 @@
 ;(function () {
     'use strict'
 
+    const articleElement = document.querySelector('#article');
+
     const id = parseInt(location.search.substring(4))
     //Список всех статей 
-    const json = localStorage.getItem('articles');
-    const articles = JSON.parse(json);
+    const json = localStorage.getItem('articles')
+    const articles = JSON.parse(json)
+
+    let article = null
+    for (let i = 0; i < articles.length; i ++) {
+
+        if (articles[i].id === id) {
+            article = articles[i]
+            }
+        console.log(article)
+    }
+
+    articleElement.innerHTML = marked.parse(article.content)
 
 })();
